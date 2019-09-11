1f8cc1813235d5bc4819bd4d04ab13a3e2448b95
Will McGIll <willmcgill@Wills-MacBook-Pro.local>
2019-09-11 16:40:21 -0400

    updated results for hours including zeros and showing hours as standard not military

--- main.js
+++ main.js
@@ -8,16 +8,35 @@ function updateTime(){
     var TimeNow = new Date();  //create variable to update
     console.log('second');
 
-//Convert military hours to standard
-
-if (TimeNow.getHours() > 12){
-    document.getElementById("Hour").innerHTML = TimeNow.getHours() - 12;}
-
-else{
-    document.getElementById("Hour").innerHTML = TimeNow.getHours();
-}
-document.getElementById("Min").innerHTML = TimeNow.getMinutes();
-document.getElementById("Sec").innerHTML = TimeNow.getSeconds();
-
-}
-
+//Hours
+    if (TimeNow.getHours() > 12){
+        if (TimeNow.getHours() - 12 < 10){
+            document.getElementById("Hour").innerHTML = '0' + (TimeNow.getHours() - 12);
+            }
+            else {
+                document.getElementById("Hour").innerHTML = TimeNow.getHours() - 12;
+            }
+    }
+    else{
+            if (TimeNow.getHours() < 10){
+            document.getElementById("Hour").innerHTML = '0' + TimeNow.getHours();
+            }
+            else {
+                document.getElementById("Hour").innerHTML = TimeNow.getHours();
+            }
+        }
+//Minutes
+    if (TimeNow.getMinutes() > 9){
+        document.getElementById("Min").innerHTML = TimeNow.getMinutes();
+        }
+    else{
+        document.getElementById("Min").innerHTML = '0' + TimeNow.getMinutes();
+        }
+//Seconds
+    if (TimeNow.getSeconds() > 9){
+        document.getElementById("Sec").innerHTML = TimeNow.getSeconds();
+        }
+    else{
+        document.getElementById("Sec").innerHTML = '0' + TimeNow.getSeconds();
+        }
+    }
